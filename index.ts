3241f556c7b3438661ad59b2d18a02f61f3815db
Kush Patel <kushpatel@Kushs-Mac.local>
2023-02-20 02:45:14 +0530

    feat: add basic requirments

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const age = parseInt(agePrompt) < 18 ? 18 : parseInt(agePrompt)
 const retirementAge = parseInt(retirementAgePrompt) > 58 ? 58 : parseInt(retirementAgePrompt)
 const currentPfBalance = parseInt(currentPfBalancePrompt) || 0
 const basicSalary = parseInt(basicSalaryPrompt) || 12000
-const incremetPersentage = parseFloat(incremetPersentagePrompt) || 1
+const incremetPersentage = parseFloat(incremetPersentagePrompt) || 0
 const pfInterestPerMonth = parseFloat(pfInterestPrompt) > 0.00 ? parseFloat(pfInterestPrompt) / 12 : 8 / 12
 
 const employeeShareEPF = (basicSalary * 12) / 100
@@ -22,9 +22,25 @@ const employerShareEPS = (basicSalary * 8.33) / 100 > 1250 ? 1250 : (basicSalary
 const employerShareEPF = employeeShareEPF - employerShareEPS
 console.log('employerShareEPF:', employerShareEPF)
 
-const totalSalary = basicSalary * ( 1 +  incremetPersentage) ^ (retirementAge-age)
+const tenure = (retirementAge - age) * 12
 
-console.log('totalSalary:', totalSalary)
+let pfBalance = currentPfBalance
+let totalContributedBalance = 0
+let salary =  basicSalary
+
+for(let i = 0; i < tenure; i++) {
+    const contribution = ((salary * 12) / 100) + ((salary * 8.33) / 100 > 1250 ? 1250 : (salary * 8.33) / 100)
+    if(i !== 0 && i % 12 === 0) {
+        salary = salary + ((salary * incremetPersentage) / 100)
+    }
+    totalContributedBalance = totalContributedBalance + contribution
+    pfBalance = pfBalance + contribution
+    pfBalance = pfBalance + ((pfBalance * pfInterestPerMonth) / 100)
+}
+
+console.log('salary:', salary)
+console.log('pfBalance:', pfBalance)
+console.log('totalContributedBalance:', totalContributedBalance)
 
 
 console.log('Age:', age, 'Retirement Age:', retirementAge, 'Current Pf Balance:', currentPfBalance, 'Basic Salary:', basicSalary, 'Incremet Persentage:', incremetPersentage, 'PF Interest per month:', pfInterestPerMonth)
